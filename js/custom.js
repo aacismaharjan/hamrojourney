a0ffa971b1eb1d09ce9281feb4d6425aebc9c924
aashish2058 <44110105+aashish2058@users.noreply.github.com>
2020-02-20 18:12:33 +0545

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$("#packAdd").fadeOut();
+	$("#gallAdd").fadeOut();
+
+	$("#packAddBtn").click(function(){
+		$("#packAdd").fadeToggle(400);
+	})
+
+	$("#gallAddBtn").click(function(){
+		$("#gallAdd").fadeToggle(400);
+	})
+
+	$("#scrollBtn").click(function(){
+		$('html, body').animate({scrollTop: 0}, 1000);
+	})
+
+})
+
+
+var scrollBtn = document.getElementById('scrollBtn');
+
+window.onscroll = function(){
+	if(document.documentElement.scrollTop <=200){
+		scrollBtn.classList.add("hide");
+		scrollBtn.classList.remove("show");
+	}else{
+		scrollBtn.classList.add("show");
+		scrollBtn.classList.remove("hide");
+	}
+}
