8396ecdac1c1ef7bdcf2eb517e97b448bb9c1d0f
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-08-06 13:38:59 +0545

    Website Optimization for Online Viewers

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,34 @@
-$(document).ready(function(){
+// Animation
+$(document).ready(function () {
 	$("#packAdd").fadeOut();
 	$("#gallAdd").fadeOut();
 
-	$("#packAddBtn").click(function(){
+	$("#packAddBtn").click(function () {
 		$("#packAdd").fadeToggle(400);
 	})
 
-	$("#gallAddBtn").click(function(){
+	$("#gallAddBtn").click(function () {
 		$("#gallAdd").fadeToggle(400);
 	})
 
-	$("#scrollBtn").click(function(){
-		$('html, body').animate({scrollTop: 0}, 1000);
+	$("#scrollBtn").click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 1000);
 	})
 
 })
 
+// Copyright Information
+document.getElementById("copyright-name").innerHTML = `5 Stars Hotel, Copyright &copy ${new Date().getFullYear()}`;
 
+
+// Scroll to top
 var scrollBtn = document.getElementById('scrollBtn');
 
-window.onscroll = function(){
-	if(document.documentElement.scrollTop <=200){
+window.onscroll = function () {
+	if (document.documentElement.scrollTop <= 200) {
 		scrollBtn.classList.add("hide");
 		scrollBtn.classList.remove("show");
-	}else{
+	} else {
 		scrollBtn.classList.add("show");
 		scrollBtn.classList.remove("hide");
 	}
